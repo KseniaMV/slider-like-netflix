9de98323c295fc360e84e53636c1d9a0280c610c
Ksenia <48721061+KseniaMV@users.noreply.github.com>
2020-05-01 17:13:36 +0300

    add new version

--- netflix/script/slider.js
+++ netflix/script/slider.js
@@ -1,19 +1,21 @@
 window.onload = function () {
     let sliderConteiner = document.querySelector(".slider_conteiner");
     let sliderItem = Array.from(document.querySelectorAll(".slider_item"));
-    let images = []; //массив из url картинок
+    let images = [];  //массив из url картинок
     let count = sliderItem.length;
-    let image = 0;
+    let image = 0; 
+    let delta = 0;
     let rightButton = document.querySelector(".right_button");
     let leftButton = document.querySelector(".left_button");
-
     scaleImage();
 
     /* формирование url картинки и добавление в массив images */
-    for (let i = 1; i <= 11; i++) {
+    for (let i = 1; i <= 10; i++) {
         let urlNumber = `url(images/desert${i}.jpg)`;
-        images.push(urlNumber);
+        images.push(urlNumber);   
     };
+
+    let count2 = images.length - 1; /*счетчик для leftbutton*/
 
     /*заполнение элементов изображением*/
     sliderItem.forEach(element => {
@@ -22,7 +24,13 @@ window.onload = function () {
 
     });
 
-    leftButton.addEventListener("click", () => {
+
+/*движение картинок по нажатию кнопки*/ 
+
+    rightButton.addEventListener("click", () => {
+        if(delta < 0){
+            count = sliderItem.length + delta;
+        }
         let deleteItem = sliderItem.shift().remove();
         let newItem = document.createElement('div');
           newItem.classList.add("slider_item");
@@ -31,12 +39,37 @@ window.onload = function () {
           sliderConteiner.append(newItem);
           sliderItem.push(newItem);
           count ++;
-
-        if (count == images.length-1) {
+        if (count == images.length) {
             count = 0;
+        };
+
+        delta ++;
+        if(delta == 10){
+            delta = 0;
         }
         scaleImage();
-
+    });
+    
+    leftButton.addEventListener("click", () => {
+    if(delta > 0){
+        count2 = delta - 1;
+       }
+        let deleteItem = sliderItem.pop().remove();
+        let newItem = document.createElement('div');
+          newItem.classList.add("slider_item");
+          newItem.classList.add("notTarget");
+          newItem.style.backgroundImage = images[count2];
+          sliderConteiner.prepend(newItem);
+          sliderItem.unshift(newItem);
+          count2 --;
+          if (count2 == -1) {
+            count2 = images.length - 1;
+        };
+        delta --;
+        if(delta == -10){
+            delta = 0;
+        };
+        scaleImage();
     });
 
 /*поведение элементов слайдера при наведении курсора и ухода курсора*/ 
